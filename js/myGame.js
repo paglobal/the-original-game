60535d270574560d39f655e5d98d20ae31375d67
paglobal <jackamoah.pa@gmail.com>
2020-06-04 23:44:23 +0000

    audio

--- js/myGame.js
+++ js/myGame.js
@@ -25,18 +25,29 @@ let darkTheme = true;
 //Audio
 window.onload = function () {
   let firstTheme = document.querySelector("#firstTheme");
+  firstTheme.load();
   firstTheme.volume = 0.4;
+  firstTheme.play();
   let secondTheme = document.querySelector("#secondTheme");
+  secondTheme.load();
   secondTheme.volume = 0.4;
   let thirdTheme = document.querySelector("#thirdTheme");
+  thirdTheme.load();
   thirdTheme.volume = 0.4;
   let bulletShot = document.querySelector("#bulletShot");
+  bulletShot.load();
   let powerUp = document.querySelector("#powerUp");
+  powerUp.load();
   let spawned = document.querySelector("#spawned");
+  spawned.load();
   let teleport = document.querySelector("#teleport");
+  teleport.load();
   let asteroidExplode = document.querySelector("#asteroidExplode");
+  asteroidExplode.load();
   let alphaShot = document.querySelector("#alphaShot");
+  alphaShot.load();
   let shockwave = document.querySelector("#shockwave");
+  shockwave.load();
 };
 
 //Game initiate
